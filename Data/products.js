3b5b4dd2a9ddf78b899a9353fa6179981efb4305
DineshCodes23 <dk1605889@gmail.com>
2024-10-04 19:12:15 +0530

    Added product data and using in Js

---
+++ Data/products.js
@@ -0,0 +1,79 @@
+const products = [{
+    name: 'Canon EOS 90D Digital SLR Camera with 18-135 is USM Lens [Black]',
+    price: '1,21,990',
+    rating: {
+        stars: 4
+    },
+    image: 'products/dslr.jpg',
+}, {
+    name: 'Olympus OMD-EM-10-Mark-IV Mirrorless Digital Camera with Lens 14-150mm f4.0-5.6-II Silver',
+    price: '1,08,990',
+    rating: {
+        stars: 4,
+    },
+    image: 'products/dslr3.jpg'
+}, {
+    name: 'Sony Alpha ZV-E10L 24.2 Mega Pixel Interchangeable-Lens Mirrorlessvlog Optical zoom Camera with 16 - 50mm Lens',
+    price: '61,489',
+    rating: {
+        stars: 4,
+    },
+    image: 'products/dslr4.jpg'
+}]
+
+
+const productITEM = [{
+    name: 'boAt Bassheads 225 in Ear Wired Earphones with Mic (Black)',
+    price: '599',
+    rating: {
+        stars: 4,
+    },
+    image: 'products/wired.jpg'
+}, {
+    name: 'Redmi Note 9 Pro (Aurora Blue, 4GB RAM, 64GB Storage)',
+    price: '13,999',
+    rating: {
+        stars: 4,
+    },
+    image: 'products/redmi.jpg'
+}, {
+    name: 'Redmi Note 9 (Pebble Grey, 4GB RAM 64GB Storage) - 48MP Quad Camera',
+    price: '11,999',
+    rating: {
+        stars: 4,
+    },
+    image: 'products/redmi2.jpg'
+}, {
+    name: 'Noise Buds VS104 Truly Wireless Earbuds with 45H of Playtime,BT v5.2 (Mint Green)',
+    price: '898',
+    rating: {
+        stars: 4,
+    },
+    image: 'products/blue2.jpg'
+}]
+
+
+const productLIST = [{
+    name: 'Filtra 4 Ply Surgical Mask with Activated Carbon (Box of 50 Masks)',
+    price: 564,
+    rating: {
+        stars: 4,
+    },
+    image: 'products/mask.jpg'
+}, {
+    name: 'Juârez GT-13 Digital LCD Guitar Bass Violin Ukulele Clip On Automatic Chromatic Tuner',
+    price: 290,
+    rating: {
+        stars: 4,
+    },
+    image: 'products/none.jpg'
+}]
+
+const productsHTML4 = [{
+    name: 'ATOMIC HABITS Hardcover – 1 January 2023',
+    price: 857,
+    rating: {
+        stars: 4,
+    },
+    image: 'products/book.jpg'
+}]
